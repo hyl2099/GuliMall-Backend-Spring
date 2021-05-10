8671e6b71241bbd428473dcdeb4dcd6012989780
hyl2099 <hyl2099@gmail.com>
2021-05-10 21:43:17 +0200

    add spring-cloud-alibaba nacos

--- product/src/main/java/com/mall/product/ProductApplication.java
+++ product/src/main/java/com/mall/product/ProductApplication.java
@@ -3,6 +3,7 @@ package com.mall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 /*
@@ -18,7 +19,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
     3）
  */
-
+@EnableDiscoveryClient
 @MapperScan("com.mall.product.dao")
 @SpringBootApplication
 public class ProductApplication {
